8bb9253d8afa5e160d05bb04cdc26b24f3c325f9
sarrahargeum <sarra.harguem@esprit.tn>
2024-09-22 15:17:54 +0100

    change localhost with app

--- src/app/demo/service/authentication.service.ts
+++ src/app/demo/service/authentication.service.ts
@@ -13,7 +13,7 @@ import { User } from '../modals/user';
 })
 export class AuthenticationService {
 
-  private baseUrl = 'http://localhost:8099/StockMnager/api/auth';
+  private baseUrl = 'http://app:8099/StockMnager/api/auth';
 
   constructor(private http: HttpClient,
           private router: Router
